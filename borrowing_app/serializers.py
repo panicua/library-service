75f880ea1d8387ef416b1e260464472cbdb5dee4
Oleksandr Moroz <molecxander@gmail.com>
2024-06-14 13:41:48 +0300

    add: improve borrowing list view possibility

--- borrowing_app/serializers.py
+++ borrowing_app/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
+from books_app.serializers import BookSerializer
 from borrowing_app.models import Borrowing
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
-        fields = ["id", "borrow_date", "expected_return_date", "actual_return_date", "books", "users"]
+        fields = ["id", "borrow_date", "expected_return_date", "actual_return_date", "books", "user"]
+
+
+class BorrowingListSerializer(BorrowingSerializer):
+    reader_email = serializers.EmailField(source="user.email", read_only=True)
+    books = serializers.SlugRelatedField(many=True, read_only=True, slug_field='title')
+
+    class Meta:
+        model = Borrowing
+        fields = ["id", "borrow_date", "expected_return_date", "actual_return_date", "books", "reader_email"]
--- borrowing_app/views.py
+++ borrowing_app/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
 from borrowing_app.models import Borrowing
-from borrowing_app.serializers import BorrowingSerializer
+from borrowing_app.serializers import BorrowingSerializer, BorrowingListSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return BorrowingListSerializer
+        return BorrowingSerializer
 
