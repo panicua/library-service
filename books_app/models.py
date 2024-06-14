b3beeb37f0e2431640bba7bc312c580c45aa14da
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-06-14 11:34:04 +0300

    add: all models.py

--- books_app/models.py
+++ books_app/models.py
@@ -1,3 +1,32 @@
+from decimal import Decimal
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverType(models.TextChoices):
+        HARD = "HARD", "Hard"
+        SOFT = "SOFT", "Soft"
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverType.choices,
+        default=CoverType.SOFT,
+    )
+    inventory = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    daily_fee = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.01'))]
+    )
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
+    def __str__(self):
+        return f"{self.title} ({self.author})"
