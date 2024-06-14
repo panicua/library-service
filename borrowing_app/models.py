4251e3aacda012a816efe7536ecb0839047230a6
Mykola Setrakov <nikset1@gmail.com>
2024-06-14 17:55:21 +0300

    fix: field book

--- borrowing_app/models.py
+++ borrowing_app/models.py
@@ -8,12 +8,14 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
-    books = models.ManyToManyField(Book, related_name='borrowings')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="borrowings")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="borrowings"
+    )
 
     class Meta:
-        verbose_name = 'borrowing'
-        verbose_name_plural = 'borrowings'
+        verbose_name = "borrowing"
+        verbose_name_plural = "borrowings"
 
     def __str__(self):
         return f"Book taken {self.borrow_date} to borrow {self.expected_return_date}"
