825805a3993aebf7ca2eefed993e32c068151c25
Serhii Khodko <sergeyhodko123@gmail.com>
2024-06-14 15:18:11 +0300

    refactor: refactored payment model

--- payment_app/models.py
+++ payment_app/models.py
@@ -1,34 +1,40 @@
+from decimal import Decimal
 from django.db import models
-
+from django.core.validators import MinValueValidator
 from borrowing_app.models import Borrowing
 
 
 class Payment(models.Model):
-    STATUS_CHOICES = (
-        (0, "PENDING"),
-        (1, "PAID")
-    )
 
-    TYPE_CHOICES = (
-        (0, "PAYMENT"),
-        (1, "FINE")
-    )
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        PAID = "PAID", "Paid"
 
-    status = models.IntegerField(
-        choices=STATUS_CHOICES, default=0
+    class Type(models.TextChoices):
+        PAYMENT = "PAYMENT", "Payment"
+        FINE = "FINE", "Fine"
+
+    status = models.CharField(
+        max_length=7,
+        choices=Status.choices,
+        default=Status.PENDING
     )
-    type = models.IntegerField(
-        choices=TYPE_CHOICES, default=0
+    type = models.CharField(
+        max_length=7,
+        choices=Type.choices,
+        default=Type.PAYMENT
     )
 
     borrowing = models.ForeignKey(
         Borrowing, on_delete=models.CASCADE, related_name="payments"
     )
 
-    session_url = models.CharField(max_length=255)
+    session_url = models.URLField(max_length=255)
     session_id = models.CharField(max_length=63)
 
-    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+    money_to_pay = models.DecimalField(max_digits=10,
+                                       decimal_places=2,
+                                       validators=[MinValueValidator(Decimal('0.0'))])
 
     def __str__(self):
         return f"Payment: {self.id}; Pay: {self.money_to_pay};"
