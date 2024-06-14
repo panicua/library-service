bb8a004ad101f3551f0c4fb669af487dc9c532f2
Mykola Setrakov <nikset1@gmail.com>
2024-06-14 17:56:41 +0300

    upd: BorrowingListSerializer; del: BorrowingDetailSerializer

--- borrowing_app/serializers.py
+++ borrowing_app/serializers.py
@@ -2,27 +2,22 @@ from rest_framework import serializers
 
 from books_app.serializers import BookSerializer
 from borrowing_app.models import Borrowing
+from user.serializers import UserSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
-        fields = ["id", "borrow_date", "expected_return_date", "actual_return_date", "books", "user"]
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        ]
 
 
 class BorrowingListSerializer(BorrowingSerializer):
-    reader_email = serializers.EmailField(source="user.email", read_only=True)
-    books = serializers.SlugRelatedField(many=True, read_only=True, slug_field='title')
-
-    class Meta:
-        model = Borrowing
-        fields = ["id", "borrow_date", "expected_return_date", "actual_return_date", "books", "reader_email"]
-
-
-class BorrowingDetailSerializer(BorrowingSerializer):
-    books = BookSerializer(many=True, read_only=True)
-    reader_email = serializers.EmailField(source="user.email", read_only=True)
-
-    class Meta:
-        model = Borrowing
-        fields = ["id", "borrow_date", "expected_return_date", "actual_return_date", "books", "reader_email"]
+    user = UserSerializer(read_only=True)
+    book = BookSerializer(read_only=True)
