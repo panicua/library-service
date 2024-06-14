e7c1e98b344c66059cc609dc3256df8b2d9ce80b
Oleksandr Moroz <molecxander@gmail.com>
2024-06-14 13:13:39 +0300

    add: null & blank parameters

--- borrowing_app/models.py
+++ borrowing_app/models.py
@@ -7,6 +7,13 @@ from books_app.models import Book
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
     books = models.ManyToManyField(Book, related_name='borrowings')
     users = models.ForeignKey(User, on_delete=models.CASCADE, related_name="borrowings")
+
+    class Meta:
+        verbose_name = 'borrowing'
+        verbose_name_plural = 'borrowings'
+
+    def __str__(self):
+        return f"Book taken {self.borrow_date} to borrow {self.expected_return_date}"
