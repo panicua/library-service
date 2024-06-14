2eb2d6ea96365cb03b66fa8a6648010532d873ea
Serhii Khodko <sergeyhodko123@gmail.com>
2024-06-14 15:21:07 +0300

    refactor: changed session_id to IntegerField

--- payment_app/models.py
+++ payment_app/models.py
@@ -30,7 +30,7 @@ class Payment(models.Model):
     )
 
     session_url = models.URLField(max_length=255)
-    session_id = models.CharField(max_length=63)
+    session_id = models.IntegerField(max_length=63)
 
     money_to_pay = models.DecimalField(max_digits=10,
                                        decimal_places=2,
