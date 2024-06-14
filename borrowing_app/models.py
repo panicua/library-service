b704a294a17c9000c96ee8afa458f812d7da6ba3
Oleksandr Moroz <molecxander@gmail.com>
2024-06-14 12:35:24 +0300

    add: models

--- borrowing_app/models.py
+++ borrowing_app/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+from user.models import User
+from books_app.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ManyToManyField(Book, on_delete=models.CASCADE, related_name='borrowings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="borrowings")
