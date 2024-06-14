5a34dec9bbef6499228cfffc75f0c14e53c2c7e2
Mykola Setrakov <nikset1@gmail.com>
2024-06-14 21:13:28 +0300

    fix: get_user_model

--- borrowing_app/models.py
+++ borrowing_app/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-from user.models import User
 from books_app.models import Book
+
+User = get_user_model()
 
 
 class Borrowing(models.Model):
