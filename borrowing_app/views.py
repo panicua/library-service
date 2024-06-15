10c1ecee174ad4d45f510bcbc6a31378da9f21a5
Oleksandr Moroz <molecxander@gmail.com>
2024-06-15 21:21:57 +0300

    add: permission

--- borrowing_app/views.py
+++ borrowing_app/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from borrowing_app.models import Borrowing
 from borrowing_app.serializers import (
@@ -10,11 +11,11 @@ from borrowing_app.serializers import (
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
             return BorrowingListSerializer
-
         return self.serializer_class
 
     def perform_create(self, serializer):
