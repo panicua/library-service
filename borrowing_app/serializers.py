05cb7f7cd379b7577a05a2c172a45520a0f2a966
Mykola Setrakov <nikset1@gmail.com>
2024-06-15 10:46:32 +0300

    add: validate_book; upd: create method, fields

--- borrowing_app/serializers.py
+++ borrowing_app/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from books_app.serializers import BookSerializer
@@ -8,16 +9,34 @@ from user.serializers import UserSerializer
 class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
-        fields = [
+        fields = ("expected_return_date", "book")
+
+    @staticmethod
+    def validate_book(value):
+        if value.inventory <= 0:
+            raise serializers.ValidationError("You can't borrow a book now")
+        return value
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            book = validated_data["book"]
+            borrowing = Borrowing.objects.create(**validated_data)
+            book.inventory -= 1
+            book.save()
+            return borrowing
+
+
+class BorrowingListSerializer(BorrowingSerializer):
+    user = UserSerializer(read_only=True)
+    book = BookSerializer(read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = (
             "id",
             "borrow_date",
             "expected_return_date",
             "actual_return_date",
             "book",
             "user",
-        ]
-
-
-class BorrowingListSerializer(BorrowingSerializer):
-    user = UserSerializer(read_only=True)
-    book = BookSerializer(read_only=True)
+        )
