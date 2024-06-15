807f611c16d28fe748276e37ccfb87e53a517480
Oleksandr Moroz <molecxander@gmail.com>
2024-06-15 21:22:10 +0300

    add: add filtering for is_active & user_id, admin see all borrowings

--- borrowing_app/views.py
+++ borrowing_app/views.py
@@ -13,6 +13,26 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     serializer_class = BorrowingSerializer
     permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        queryset = self.queryset
+        user = self.request.user
+
+        if not user.is_staff:
+            queryset = queryset.filter(user=user)
+        else:
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(user__id=user_id)
+
+        is_active = self.request.query_params.get("is_active")
+        if is_active is not None:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.exclude(actual_return_date__isnull=True)
+
+        return queryset
+
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
             return BorrowingListSerializer
