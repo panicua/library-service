108748d30dcdc1887d2e9b073920a60e744701cb
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-06-17 17:20:18 +0300

    ref: refactor all tests files

--- books_app/tests/test_book_app_model.py
+++ books_app/tests/test_book_app_model.py
@@ -1,10 +1,12 @@
 from decimal import Decimal
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from books_app.models import Book
 
 
 class BookModelTest(TestCase):
     def setUp(self):
+        """Set up a book instance for testing the model."""
         self.book = Book.objects.create(
             title="Test Book",
             author="Test Author",
@@ -14,6 +16,7 @@ class BookModelTest(TestCase):
         )
 
     def test_book_creation(self):
+        """Test the creation of a book instance."""
         self.assertEqual(self.book.title, "Test Book")
         self.assertEqual(self.book.author, "Test Author")
         self.assertEqual(self.book.cover, Book.CoverType.SOFT)
@@ -21,4 +24,29 @@ class BookModelTest(TestCase):
         self.assertEqual(self.book.daily_fee, Decimal("1.99"))
 
     def test_str_representation(self):
+        """Test the string representation of a book instance."""
         self.assertEqual(str(self.book), "Test Book (Test Author)")
+
+    def test_inventory_positive(self):
+        """Test that inventory is positive."""
+        book = Book(
+            title="Test Book",
+            author="Test Author",
+            cover=Book.CoverType.SOFT,
+            inventory=-1,
+            daily_fee=Decimal("1.99")
+        )
+        with self.assertRaises(ValidationError):
+            book.full_clean()
+
+    def test_daily_fee_positive(self):
+        """Test that daily fee is positive."""
+        book = Book(
+            title="Test Book",
+            author="Test Author",
+            cover=Book.CoverType.SOFT,
+            inventory=10,
+            daily_fee=Decimal("-1.99")
+        )
+        with self.assertRaises(ValidationError):
+            book.full_clean()
--- books_app/tests/test_book_app_serializer.py
+++ books_app/tests/test_book_app_serializer.py
@@ -6,6 +6,7 @@ from books_app.serializers import BookSerializer, BookListSerializer
 
 class BookSerializerTest(TestCase):
     def setUp(self):
+        """Set up a book instance for testing serializers."""
         self.book_attributes = {
             "title": "Test Book",
             "author": "Test Author",
@@ -16,6 +17,7 @@ class BookSerializerTest(TestCase):
         self.book = Book.objects.create(**self.book_attributes)
 
     def test_book_serializer(self):
+        """Test the BookSerializer for the correct output data."""
         serializer = BookSerializer(self.book)
         data = serializer.data
         self.assertEqual(set(data.keys()), set(["id", "title", "author", "cover", "inventory", "daily_fee"]))
@@ -26,6 +28,7 @@ class BookSerializerTest(TestCase):
         self.assertEqual(data["daily_fee"], str(self.book_attributes["daily_fee"]))
 
     def test_book_list_serializer(self):
+        """Test the BookListSerializer for the correct output data."""
         serializer = BookListSerializer(self.book)
         data = serializer.data
         self.assertEqual(set(data.keys()), set(["id", "title", "author", "inventory", "daily_fee"]))
