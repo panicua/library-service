0acdcf61e2bc4606203907e679acf24430579b6a
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-06-17 17:18:49 +0300

    add: test for admin panel

---
+++ books_app/tests/test_book_app_admin.py
@@ -0,0 +1,7 @@
+from django.contrib.admin.sites import site
+from django.test import TestCase
+from books_app.models import Book
+
+class BookAdminTest(TestCase):
+    def test_book_admin_registered(self):
+        self.assertIn(Book, site._registry)
